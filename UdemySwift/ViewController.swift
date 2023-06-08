d79ddddcd55206e2f5b3d0f4d24b34cacd8178bf
Ronalyn Rosales <ronnajrosales@gmail.com>
2023-06-08 13:14:13 +0800

    Autolayout and Buttons

--- UdemySwift/ViewController.swift
+++ UdemySwift/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+ 
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordButton.isEnabled = false
+    }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in Progress"
+        stopRecordButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Tap to Record"
+        stopRecordButton.isEnabled = false
+        recordButton.isEnabled = true
+     
+    
+    }
+    
 }
 
